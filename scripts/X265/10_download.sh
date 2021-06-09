1f5cdf33e10031c6e50f095aa4c08031541701f2
Josh.5 <jsunnex@gmail.com>
2021-06-09 19:02:26 +1200

    Bump x265 to v3.5 and improve make install

--- scripts/X265/10_download.sh
+++ scripts/X265/10_download.sh
@@ -4,13 +4,13 @@
 # File Created: Wednesday, 9th June 2021 10:15:21 am
 # Author: Josh.5 (jsunnex@gmail.com)
 # -----
-# Last Modified: Wednesday, 9th June 2021 10:16:25 am
+# Last Modified: Wednesday, 9th June 2021 6:22:50 pm
 # Modified By: Josh.5 (jsunnex@gmail.com)
 ###
 
 echo "**** grabbing x265 (${X265}) ****"
 mkdir -p /tmp/x265
 curl -Lf \
-    https://github.com/videolan/x265/archive/Release_${X265}.tar.gz | \
+    https://bitbucket.org/multicoreware/x265_git/downloads/x265_${X265}.tar.gz | \
     tar -zx --strip-components=1 -C /tmp/x265
 [[ $? -gt 0 ]] && exit 1
--- scripts/X265/20_build.sh
+++ scripts/X265/20_build.sh
@@ -4,28 +4,66 @@
 # File Created: Wednesday, 9th June 2021 10:15:49 am
 # Author: Josh.5 (jsunnex@gmail.com)
 # -----
-# Last Modified: Wednesday, 9th June 2021 10:15:54 am
+# Last Modified: Wednesday, 9th June 2021 6:32:11 pm
 # Modified By: Josh.5 (jsunnex@gmail.com)
 ###
 
-echo "**** compiling x265 ****"
+echo "**** compiling x265 (ARCH: $(uname -m)) ****"
 if uname -m | grep -q x86; then
+    # x86
     cd /tmp/x265/build/linux
-    export MAKEFLAGS="-j$(nproc) "
     ./multilib.sh
     [[ $? -gt 0 ]] && exit 1
-    echo
-else
+    make -C 8bit install
+    [[ $? -gt 0 ]] && exit 1
+elif uname -m | grep -q aarch64; then
+    # arm64
     cd /tmp/x265/build/linux
     export CXXFLAGS="-fPIC"
-    export MAKEFLAGS="-j$(nproc) "
     ./multilib.sh
     [[ $? -gt 0 ]] && exit 1
-    echo
+    make -C 8bit install
+    [[ $? -gt 0 ]] && exit 1
+elif uname -m | grep -q armv7l; then
+    # armv7
+    cd /tmp/x265/build/linux
+    curl -fL \
+        https://sources.debian.org/data/main/x/x265/3.4-2/debian/patches/0001-Fix-arm-flags.patch \
+        > arm.patch
+    patch ../../source/CMakeLists.txt < arm.patch
+    cmake \
+        -D ENABLE_ASSEMBLY=OFF \
+        -D ENABLE_CLI=OFF \
+        -G "Unix Makefiles" ../../source
+    make
+    [[ $? -gt 0 ]] && exit 1
+    make install
+    [[ $? -gt 0 ]] && exit 1
 fi
 
-# Write install script
+# Write install script (different for different arch)
+# x86
+if uname -m | grep -q x86; then
 cat << 'EOF' > /tmp/x265/install-cmd.sh
-cd /tmp/x265/build/linux && make -C 8bit install
+cd /tmp/x265/build/linux
+make -C 8bit install
 if [ $? -gt 0 ]; then exit 1; fi
 EOF
+fi
+# arm64
+if uname -m | grep -q aarch64; then
+cat << 'EOF' > /tmp/x265/install-cmd.sh
+cd /tmp/x265/build/linux
+export CXXFLAGS="-fPIC"
+make -C 8bit install
+if [ $? -gt 0 ]; then exit 1; fi
+EOF
+fi
+# armv7
+if uname -m | grep -q armv7l; then
+cat << 'EOF' > /tmp/x265/install-cmd.sh
+cd /tmp/x265/build/linux
+make install
+if [ $? -gt 0 ]; then exit 1; fi
+EOF
+fi
